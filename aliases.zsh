40282a6ca0c2dfacb21aa7240e22838135a46169
Damjan Marion <damjan.marion@gmail.com>
2024-12-17 22:40:56 +0100

    Update aliases.zsh

--- aliases.zsh
+++ aliases.zsh
@@ -1,3 +1,3 @@
-alias key="keychain $(find ~/.ssh/id_* | grep -v '.pub$') && . ~/.keychain/$HOST-sh"
+alias key="keychain \$(ls ~/.ssh/id_* | grep -v '.pub$') && . ~/.keychain/$HOST-sh"
 alias ssh-keygen-ed25519="ssh-keygen -t ed25519 -C ${USER}@${HOST}"
 alias update_authorized_keys="curl https://github.com/${USER}.keys | tee ${HOME}/.ssh/authorized_keys"
